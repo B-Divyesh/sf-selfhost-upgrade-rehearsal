fcf5d85ef7f26dba41d2a8ccd8e418eebf13115d
Param Factory <factory@sociobot.in>
2026-08-29 19:48:30 +0000

    ci: authenticate Homebrew tap push

--- e2e/fixtures/homebrew-formula-v0.1.3.rb
+++ e2e/fixtures/homebrew-formula-v0.1.3.rb
@@ -7,16 +7,16 @@ class Rehearsal < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/B-Divyesh/sf-selfhost-upgrade-rehearsal/releases/download/v#{version}/rehearsal-macos-aarch64.tar.gz"
-      sha256 "86304dfa1b0bc1a750a3244751d7c5355b0b11a7621a8f95748d64bdcbcef7a6"
+      sha256 "f3a88c14a9f809906c50b2c81f75809ffabd4753a9d7fb30ed35fb53309bb383"
     else
       url "https://github.com/B-Divyesh/sf-selfhost-upgrade-rehearsal/releases/download/v#{version}/rehearsal-macos-x86_64.tar.gz"
-      sha256 "c2ae05e0b69816c912ab85ca5c65bfd21670a2b445eccf943d11d9f7eb58b936"
+      sha256 "57ff035c2caf979714bbbce75454be56787976acfee6cf7d96a2310cb4c634a3"
     end
   end
 
   on_linux do
     url "https://github.com/B-Divyesh/sf-selfhost-upgrade-rehearsal/releases/download/v#{version}/rehearsal-linux-x86_64.tar.gz"
-    sha256 "baae9909cffe06c7d5a3ba0f0f50eb2c8678a02461eb89fe05c1e2f6152f3f3a"
+    sha256 "84676836384b1d87e7a590147989656f82e77d6e2c73e24222cf984c7d8e03ca"
   end
 
   def install
